3b80989d9b51424889aed64a400170a6152ed59e
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-29 20:04:07 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,17 +11,19 @@ basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
     distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    if (distancia < 10) {
+    if (distancia < 20) {
         maqueen.motorStop(maqueen.Motors.All)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
         basic.pause(1000)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
         basic.pause(1000)
     }
 })
