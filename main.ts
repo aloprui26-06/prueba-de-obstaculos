d9f9e9738dffbc20448825d611738270123067eb
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-29 16:32:03 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,27 @@
 let distancia = 0
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    `)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
     distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
-    if (distancia < 5) {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    if (distancia < 10) {
         maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         basic.pause(1000)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         basic.pause(1000)
     }
 })
